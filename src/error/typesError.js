96288000b95fab0f46abd16fa921a4216578e6cc
aegarmus <aegarmus@gmail.com>
2024-11-20 20:44:48 -0300

    feat: Error Handler created

---
+++ src/error/typesError.js
@@ -0,0 +1,26 @@
+import AppError from "./AppError.js";
+
+export class ValidationError extends AppError {
+    constructor(message, details) {
+        super(message || 'Error de Validación', 400, details)
+    }
+}
+
+export class NotFoundError extends AppError {
+    constructor(message, details, entity) {
+        super(message || `${entity} No encontrado`, 404, details)
+    }
+}
+
+export class YeisonError extends AppError {
+    constructor(message, details) {
+        super(message || 'Error en el JSON de datos', 500, details)
+    }
+}
+
+export class InternalServerError extends AppError {
+    constructor(message, details) {
+        super(message || 'Error interno del Servidor', 500, details)
+    }
+}
+
